147ea146ff36be999db1b3290122cf1e54a87c32
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-15 15:57:43 +0200

    ДЗ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+/* 1. Создание и рендер разметки по массиву данных 
+galleryItems и предоставленному шаблону элемента галереи.*/
+
+function createMarkup(pictures) {
+  console.log(pictures.original);
+  //   return `
+  // <div class="gallery__item">
+  //     <a class="gallery__link" href="${original}">
+  //         <img
+  //             class="gallery__image"
+  //             src="${preview}"
+  //             data-source="${original}"
+  //             alt="${description}"
+  //         />
+  //     </a>
+  // </div>`;
+}
+
+// console.log(createMarkup(galleryItems));
+createMarkup(galleryItems);
