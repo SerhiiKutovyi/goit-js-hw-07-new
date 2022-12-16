781d189e9bd9db74c81f62ba97d1eaf5e3bbd629
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-16 22:42:47 +0200

    ДЗ-7 1/2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// console.log(galleryItems);
+
+/* 1. Создание и рендер разметки по массиву данных
+          galleryItems и предоставленному шаблону элемента галереи.*/
 
 const galleryRef = document.querySelector('.gallery');
 
-/* 1. Создание и рендер разметки по массиву данных 
-galleryItems и предоставленному шаблону элемента галереи.*/
-
 function createMarkup(pictures) {
-  console.log(pictures.original);
-  //   return `
-  // <div class="gallery__item">
-  //     <a class="gallery__link" href="${original}">
-  //         <img
-  //             class="gallery__image"
-  //             src="${preview}"
-  //             data-source="${original}"
-  //             alt="${description}"
-  //         />
-  //     </a>
-  // </div>`;
+  return pictures
+    .map(({ original, preview, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}
+galleryRef.innerHTML = createMarkup(galleryItems);
+
+createMarkup(galleryItems);
+
+/* 2. Реализация делегирования на div.gallery 
+                      и получение url большого изображения.*/
+
+galleryRef.addEventListener('click', onClickBigPicture);
+
+function onClickBigPicture(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg" width="800" height="600">
+    
+`);
+  console.log(instance);
+
+  instance.show();
+  // console.log(event.target.dataset.source);
 }
 
-// console.log(createMarkup(galleryItems));
-createMarkup(galleryItems);
+/* 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
+Используй CDN сервис jsdelivr и добавь в
+ проект ссылки на минифицированные (.min) файлы библиотеки.*/
