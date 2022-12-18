fc9ee369c22ddc3cdbf0f0564f7258cd82218a67
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-18 15:47:37 +0200

    ДЗ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 
 /* 1. Создание и рендер разметки по массиву данных
-          galleryItems и предоставленному шаблону элемента галереи.*/
+galleryItems и предоставленному шаблону элемента галереи.*/
 
 const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onClickBigPicture);
 
 function createMarkup(pictures) {
   return pictures
@@ -27,23 +29,27 @@ galleryRef.innerHTML = createMarkup(galleryItems);
 createMarkup(galleryItems);
 
 /* 2. Реализация делегирования на div.gallery 
-                      и получение url большого изображения.*/
-
-galleryRef.addEventListener('click', onClickBigPicture);
+и получение url большого изображения.*/
 
 function onClickBigPicture(event) {
   event.preventDefault();
 
-  const instance = basicLightbox.create(`
-    <img src="https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg" width="800" height="600">
-    
-`);
-  console.log(instance);
+  const img = event.target.dataset.source;
 
-  instance.show();
-  // console.log(event.target.dataset.source);
+  if (event.target !== event.currentTarget) {
+    const instance = basicLightbox.create(`
+      <img src="${img}" width="800" height="600">
+  `);
+    instance.show();
+
+    galleryRef.addEventListener('keydown', eve => {
+      if (eve.code === 'Escape') {
+        instance.close();
+      }
+    });
+  }
 }
 
 /* 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
 Используй CDN сервис jsdelivr и добавь в
- проект ссылки на минифицированные (.min) файлы библиотеки.*/
+проект ссылки на минифицированные (.min) файлы библиотеки.*/
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createMarkup(items) {
+  return items
+    .map(ele => {
+      return `<div class="gallery">
+      <a class="gallery__item" href="${ele.original}">
+  <img class="gallery__image" src="${ele.preview}" alt="${ele.description}" />
+</a>
+</div> `;
+    })
+    .join('');
+}
+
+galleryRef.innerHTML = createMarkup(galleryItems);
+
+createMarkup(galleryItems);
+
+galleryRef.addEventListener('click', openItemsClick);
+
+function openItemsClick(event) {
+  event.preventDefault();
+
+  if (event.target !== event.currentTarget) {
+    new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+}
